37e43b500a3fa7c4d34d88f59cc94ea074051917
MyuW-who <pooh511776@gmail.com>
2025-11-10 15:58:34 +0700

    change name

--- view_Global/js/menu.js
+++ view_Global/js/menu.js
@@ -19,6 +19,12 @@ information_btn1?.addEventListener('click', () => {
   window.electronAPI.navigate('information1');
 });
 
+const information_btn2 = document.getElementById('information-btn2');
+information_btn2?.addEventListener('click', () => {
+  window.electronAPI.navigate('information2');
+});
+
+
 const dashboard_btn = document.getElementById('dashboard-btn');
 dashboard_btn?.addEventListener('click', () => {
   window.electronAPI.navigate('dashboard1');
